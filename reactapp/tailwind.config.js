85016779bc1d04b1ab15e65ed0577050b65badd4
olivertrain1996@gmail.com <olivertrain1996@gmail.com>
2023-08-30 13:57:41 +0100

    Set up repository with defined built tailwind classes

--- reactapp/tailwind.config.js
+++ reactapp/tailwind.config.js
@@ -4,9 +4,46 @@ export default {
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'class',
+    theme: {
+        screens: {
+            xs: '552px',
+            sm: '768px',
+            md: '1024px',
+            lg: '1200px',
+            xl: '1440px',
+        },
+        extend: {
+            colors: {
+                lightmodeBacking: '#141625',
+                darkmodeBacking: '#FFFFFF',
+                buttonPurple: '#7C5DFA',
+                hoverPurple: '#9277FF',
+                editlight: '#F9FAFE',
+                editDark: '#252945',
+                buttonHoverLight: '#DFE3FA',
+                buttonGrey: '#373B53',
+                buttonHover: '#0C0E16',
+                redDelete: '#EC5757',
+                redDeleteHover: '#FF9797',
+                newitemDef: '#F8F8FB',
+                greyText: '#7E88C3',
+                bgInputDark: '#1E2139',
+                borderInputDark: '#1E2139',
+            },
+            fontFamily: {
+                serif: ['serif'],
+            },
+            fontSize: {
+                '2xl': '12.5rem',
+            },
+            borderRadius: {
+                'lg': '1.5rem'
+            },
+            borderWidth: {
+                '1': '1px'
+            }
+        },
+    },
+    plugins: [],
 }
-
